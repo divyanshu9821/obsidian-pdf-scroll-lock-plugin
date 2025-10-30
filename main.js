16e74aebdd3df246af7f0165dea9660aa6e65873
divyanshu9821 <singhaldivyanshu2001@gmail.com>
2025-10-30 12:27:05 +0530

    added light and dark theme icon based on the obsidian theme

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const { Plugin } = require('obsidian')
 
+const lockedIcon = `<svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z" stroke="#1C274C" stroke-width="3"/><path d="M6 10V8C6 4.68629 8.68629 2 12 2C15.3137 2 18 4.68629 18 8V10" stroke="#1C274C" stroke-width="3" stroke-linecap="round"/></svg>`
+const unlockedIcon = `<svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16Z" stroke="#1C274C" stroke-width="3"/><path d="M6 10V8C6 4.68629 8.68629 2 12 2" stroke="#1C274C" stroke-width="3" stroke-linecap="round"/></svg>`
+
+function updateTheme() {
+    const isDark = document.body.classList.contains('theme-dark');
+    const strokeColor = isDark ? '#FFFFFF' : '#1C274C';
+    document.querySelectorAll('.scroll-toggle-btn svg path')
+        .forEach(p => p.setAttribute('stroke', strokeColor));
+}
+
 function setLockingMechanism(element) {
     const container = element.querySelector('.pdf-viewer-container')
     const toolbar = element.querySelector('.pdf-toolbar-left')
@@ -8,11 +18,13 @@ function setLockingMechanism(element) {
     if (element.querySelector('.scroll-toggle-btn')) return
 
     const button = document.createElement("button")
-    button.textContent = "🔒"
+    button.innerHTML = lockedIcon
+    // button.innerHTML = "🔒"
     button.style.fontSize = "1rem"
     button.style.padding = "0px"
     button.style.background = "none"
     button.style.opacity = "0.6"
+    button.style.boxShadow = "none"
     button.setAttribute('aria-label', "toggle scrolling")
 
     button.classList.add('scroll-toggle-btn') // helpful in handling duplicacy
@@ -24,13 +36,15 @@ function setLockingMechanism(element) {
         locked = !locked
         container.style.overflow = locked ? "hidden" : "auto"
         container.style.pointerEvents = locked ? "none" : "auto";
-        button.textContent = locked ? "🔒" : "🔓"
+        button.innerHTML = locked ? lockedIcon : unlockedIcon
     }
 
     toggle()
 
     button.addEventListener("click", toggle)
     toolbar.prepend(button)
+
+    updateTheme();
 }
 
 function removeLockingMechanism() {
@@ -49,6 +63,14 @@ function removeLockingMechanism() {
 module.exports = class LockPDF extends Plugin {
 
     onload() {
+
+        this.registerEvent(
+            this.app.workspace.on('css-change', () => {
+                updateTheme();
+            })
+        );
+
+
         document.querySelectorAll('.pdf-embed').forEach(setLockingMechanism)
 
         this.observer = new MutationObserver(mutations => {
